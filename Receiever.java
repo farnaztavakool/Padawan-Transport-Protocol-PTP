b24cc0491138dc76efefbcbefa3e86385dad2915
farnaztavakool <tavakolfarnaz@gmail.com>
2021-07-30 00:16:29 +1000

    latest changes

--- Receiever.java
+++ Receiever.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Receiever {
     // inputs :receiver_port FileReceived.txt
@@ -12,11 +13,13 @@ public class Receiever {
     private final PTP PTP_send;
 
     public Receiever(int port_number, String path) throws Exception {
+        System.out.println("listening");
         IP = InetAddress.getByName("localhost");
         this.PTP_send = new PTP(port, IP.toString());
         this.port = port_number;
         openFile(path);
         createSocket();
+        connect();
         listen();
 
     }
@@ -34,17 +37,18 @@ public class Receiever {
      * @return
      */
     public void connect() throws Exception {
+        System.out.println("Listening for connection");
         while (true) {
             byte[] receiveData = new byte[1024];
             // receive from server
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             clientSocket.receive(receivePacket);
             HashMap<String, String> packet = PTP.receive_PTP_packet(receivePacket);
-
+            System.out.println(packet);
             if (PTP.get_flag(packet).equals("SYN")) {
-                byte[] send = PTP_send.send_ACK(true);
+                byte[] send = PTP_send.send_ACK(true, packet);
                 DatagramPacket send_packet = new DatagramPacket(send, send.length,
-                        InetAddress.getByName(packet.get("IP")), Integer.parseInt(packet.get("")));
+                        InetAddress.getByName(packet.get("IP").split("/")[0]), Integer.parseInt(packet.get("port")));
                 clientSocket.send(send_packet);
             }
             byte[] final_ACK = new byte[1024];
@@ -53,8 +57,11 @@ public class Receiever {
             clientSocket.receive(finalPacket);
             packet = PTP.receive_PTP_packet(finalPacket);
             if (PTP.get_flag(packet).equals("ACK")) {
-                if (PTP_send.ACK(packet.get("ACK_number")))
-                    return;
+                System.out.println("connected");
+                if (PTP_send.ACK(Integer.parseInt(packet.get("ACK_number"))))
+                    ;
+
+                return;
             }
         }
 
@@ -67,11 +74,15 @@ public class Receiever {
     private void createSocket() throws Exception {
 
         SocketAddress sockaddr = new InetSocketAddress(IP, port);
-        clientSocket = new DatagramSocket();
+        clientSocket = new DatagramSocket(null);
         clientSocket.bind(sockaddr);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Scanner myObj = new Scanner(System.in); // Create a Scanner object
 
+        String[] input = myObj.nextLine().split(" ");
+        System.out.println(input);
+        Receiever receiver = new Receiever(Integer.parseInt(input[0]), input[1]);
     }
 }
--- Sender.java
+++ Sender.java
@@ -1,3 +1,87 @@
-public class S
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Sender {
+    private File file;
+    private DatagramSocket clientSocket;
+    private int port;
+    private InetAddress IP;
+    private final PTP PTP_send;
+    private final int receiver_port;
+    private final InetAddress receiver_IP;
+    private final int MWS;
+    private final int MSS;
+    private final int timeout;
+    private final int pdrop;
+
+    private final int seed;
+
+    public Sender(String receiver_IP, int receiver_port, String path, int MWS, int MSS, int timeout, int pdrop,
+            int seed) throws Exception {
+
+        this.IP = InetAddress.getByName("localhost");
+        this.port = 3003;
+        this.receiver_IP = InetAddress.getByName(receiver_IP);
+        this.receiver_port = receiver_port;
+        this.MWS = MWS;
+        this.MSS = MSS;
+        this.timeout = timeout;
+        this.pdrop = pdrop;
+        this.seed = seed;
+        createSocket();
+        this.PTP_send = new PTP(port, IP.toString());
+        connect();
+
+    }
+
+    private void openFile(String path) {
+
+        File file = new File(path);
+        this.file = file;
+
+    }
+
+    private void createSocket() throws Exception {
+
+        SocketAddress sockaddr = new InetSocketAddress(IP, port);
+        clientSocket = new DatagramSocket(null);
+        clientSocket.bind(sockaddr);
+    }
+
+    public void connect() throws Exception {
+        while (true) {
+            System.out.println("Sending SYN");
+            byte[] sendData = PTP_send.send_SYN();
+
+            DatagramPacket packet = new DatagramPacket(sendData, sendData.length, receiver_IP, receiver_port);
+            clientSocket.send(packet);
+
+            byte[] receieveData = new byte[1024];
+            packet = new DatagramPacket(receieveData, receieveData.length);
+            clientSocket.receive(packet);
+            HashMap<String, String> packet_recieved = PTP.receive_PTP_packet(packet);
+            if (PTP.get_flag(packet_recieved).equals("SYN/ACK")) {
+                System.out.println("receieved SYN/ACK sending ACK");
+                byte[] send = PTP_send.send_ACK(false, packet_recieved);
+                DatagramPacket send_packet = new DatagramPacket(send, send.length, receiver_IP, receiver_port);
+                clientSocket.send(send_packet);
+                System.out.println("successfully connected");
+                return;
+
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner myObj = new Scanner(System.in); // Create a Scanner object
+
+        String[] input = myObj.nextLine().split(" ");
+        Sender sender = new Sender(input[0], Integer.parseInt(input[1]), input[2], Integer.parseInt(input[3]),
+                Integer.parseInt(input[4]), Integer.parseInt(input[5]), Integer.parseInt(input[6]),
+                Integer.parseInt(input[7]));
+    }
 
 }
